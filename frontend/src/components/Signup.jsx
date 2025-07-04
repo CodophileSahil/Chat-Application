45ee51739504da2094dd1281fc8cd1fd8e974fc9
Sahil Saxena <saxenasahil718@gmail.com>
2025-07-04 17:40:53 +0530

    third commit

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding "></div>
+    </div>
+  );
+};
+
+export default Signup;
